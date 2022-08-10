2b3df9d7dbb8b53bc322331fa26d7dce71730475
AlexeyGulis <kiri.dh@yandex.ru>
2022-08-10 14:13:05 +0300

    create models sensor and measurement

--- src/main/java/by/gulis/project3API/models/Measurement.java
+++ src/main/java/by/gulis/project3API/models/Measurement.java
@@ -1,4 +1,24 @@
 package by.gulis.project3API.models;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Entity
+@Table(name = "Measurement")
 public class Measurement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "value")
+    @Min(value = -100)
+    @Max(value = 100)
+    private Double value;
+    @Column(name = "raining")
+    private Boolean raining;
+    @ManyToOne
+    @JoinColumn(name = "sensor_id", referencedColumnName = "id")
+    private Sensor owner;
 }
--- src/main/java/by/gulis/project3API/models/Sensor.java
+++ src/main/java/by/gulis/project3API/models/Sensor.java
@@ -1,6 +1,8 @@
 package by.gulis.project3API.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 @Table(name = "Sensor")
@@ -9,8 +11,43 @@ public class Sensor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(name = "name")
-    private String name;
-    
 
+    @Column(name = "name")
+    @Size(min = 2, max = 30,
+            message = "Название сенсора должно быть длиной от 2 до 30 символов")
+    private String name;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Measurement> measurementList;
+
+    public Sensor() {
+    }
+
+    public Sensor(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Measurement> getMeasurementList() {
+        return measurementList;
+    }
+
+    public void setMeasurementList(List<Measurement> measurementList) {
+        this.measurementList = measurementList;
+    }
 }
