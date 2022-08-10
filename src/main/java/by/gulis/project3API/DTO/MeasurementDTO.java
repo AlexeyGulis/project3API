afc3db3963e32212253c5a68580ff38cecaf6110
AlexeyGulis <kiri.dh@yandex.ru>
2022-08-10 14:33:33 +0300

    create DTO`s

---
+++ src/main/java/by/gulis/project3API/DTO/MeasurementDTO.java
@@ -0,0 +1,31 @@
+package by.gulis.project3API.DTO;
+
+public class MeasurementDTO {
+    private Double value;
+    private Boolean raining;
+    private SensorDTO sensorDTO;
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Boolean getRaining() {
+        return raining;
+    }
+
+    public void setRaining(Boolean raining) {
+        this.raining = raining;
+    }
+
+    public SensorDTO getSensorDTO() {
+        return sensorDTO;
+    }
+
+    public void setSensorDTO(SensorDTO sensorDTO) {
+        this.sensorDTO = sensorDTO;
+    }
+}
---
+++ src/main/java/by/gulis/project3API/DTO/SensorDTO.java
@@ -0,0 +1,13 @@
+package by.gulis.project3API.DTO;
+
+public class SensorDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
